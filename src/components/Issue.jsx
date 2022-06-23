9d2c1ec173f669289439df9cb7baeaa6e82a4851
Kyle Huskey <kylehuskey@gmail.com>
2022-06-23 09:49:49 -0400

    added styling

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -6,13 +6,20 @@ import IssueList from './IssueList'
 function Issue(props) {
   // const title = props.issue.title
   // const user = props.issue.user
-  const { title, user, number, created_at} = props.issue
+  const { title, user, number, created_at, labels} = props.issue
   const formattedDate = new Date(created_at)
 // console.log(test.id, 12)
   return (
     
     <div className={styles.issue}>
       <h4><a href={props.issue.html_url}>{title}</a></h4>
+      <div>
+        {labels.map(function(label, i){
+            return(
+            <span>{label.name}</span>
+            )
+        })}
+      </div>
       <div className={styles.issueMeta}>
         #{number} opened at {formattedDate.toLocaleString()} by <a href={user.html_url}>{user.login}</a>
       </div>
